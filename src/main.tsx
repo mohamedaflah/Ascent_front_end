c9e35a7c593730f59df9258c4b48fa78aba3acde
Aflu <mohamedaflah186@gmail.com>
2024-08-03 10:28:12 +0530

    feat: done

--- src/main.tsx
+++ src/main.tsx
@@ -5,24 +5,27 @@ import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "./shadcn/theme-provider.tsx";
 import { Provider } from "react-redux";
-import store from "./redux/store.ts";
+import { persistor, store } from "./redux/store.ts";
 import { Toaster } from "react-hot-toast";
 import { PrimeReactProvider } from "primereact/api";
 import { NextUIProvider } from "@nextui-org/react";
 import { SocketProvider } from "./contexts/SocketContext.tsx";
+import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
   <Router>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Provider store={store}>
-        <Toaster position="top-center" />
-        <PrimeReactProvider>
-          <SocketProvider>
-            <NextUIProvider>
-              <App />
-            </NextUIProvider>
-          </SocketProvider>
-        </PrimeReactProvider>
+        <PersistGate loading={null} persistor={persistor}>
+          <Toaster position="top-center" />
+          <PrimeReactProvider>
+            <SocketProvider>
+              <NextUIProvider>
+                <App />
+              </NextUIProvider>
+            </SocketProvider>
+          </PrimeReactProvider>
+        </PersistGate>
       </Provider>
     </ThemeProvider>
   </Router>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,7 @@
-import { configureStore } from "@reduxjs/toolkit";
+// store.js
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import userReducer from "./reducers/userReducer";
 import adminReducer from "./reducers/adminReducer";
 import categoryReducer from "./reducers/categoryReducer";
@@ -6,18 +9,41 @@ import jobReducer from "./reducers/jobReducer";
 import chatReducer from "./reducers/chatReducer";
 import messageReducer from "./reducers/messageReducer";
 
-const store = configureStore({
-  reducer: {
-    userData: userReducer,
-    admin: adminReducer,
-    category: categoryReducer,
-    job: jobReducer,
-    chats: chatReducer,
-    message: messageReducer,
-  },
+// Combine your reducers
+const rootReducer = combineReducers({
+  userData: userReducer,
+  admin: adminReducer,
+  category: categoryReducer,
+  job: jobReducer,
+  chats: chatReducer,
+  message: messageReducer,
 });
 
+// Persist configuration
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// Create a persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure the store
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
+});
+
+// Create the persistor
+const persistor = persistStore(store);
+
+// Export types
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-export default store;
+export { store, persistor };
